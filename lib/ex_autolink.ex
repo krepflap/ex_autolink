515db3fd1edfecafc870cc21bf05bcd34af8cfe7
krepflap <krepflap@tuta.io>
2019-06-30 12:07:26 +0200

    mix new

---
+++ lib/ex_autolink.ex
@@ -0,0 +1,18 @@
+defmodule ExAutolink do
+  @moduledoc """
+  Documentation for ExAutolink.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAutolink.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_autolink_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAutolinkTest do
+  use ExUnit.Case
+  doctest ExAutolink
+
+  test "greets the world" do
+    assert ExAutolink.hello() == :world
+  end
+end
