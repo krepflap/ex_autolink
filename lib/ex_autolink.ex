0ac6a9ae2fa52952f05c61660bdd2e3f01f8c5dd
krepflap <krepflap@tuta.io>
2019-06-30 22:16:00 +0200

    WIP implementation

--- lib/ex_autolink.ex
+++ lib/ex_autolink.ex
@@ -1,18 +1,34 @@
 defmodule ExAutolink do
   @moduledoc """
-  Documentation for ExAutolink.
+  This simple module is used for converting http(s):// links in text to
+  HTML hyperlinks.
+
+  It doesn't depend on Phoenix.HTML, but can be used in conjuction with Phoenix.
   """
 
   @doc """
-  Hello world.
+  This method is used for parsing strings or text blocks. We convert all links
+  starting with http:// or https:// to HTML links.
 
   ## Examples
 
-      iex> ExAutolink.hello()
-      :world
+      iex> ExAutolink.link("Go here: https://elixir-lang.org/.")
+      "Go here: <a href=\"https://elixir-lang.org/\">https://elixir-lang.org/</a>."
 
   """
-  def hello do
-    :world
+  def link(""), do: ""
+
+  def link(text) do
+    parse = fn uri, url ->
+      ~s(<a href="#{uri}#{url}">#{uri}#{url}</a>)
+    end
+
+    Regex.replace(~r{(https?://)([^\s\<]+)}, text, parse.("\\1", "\\2"))
+  end
+
+  defp reverse(binary) do
+    binary
+    |> :binary.decode_unsigned(:little)
+    |> :binary.encode_unsigned(:big)
   end
 end
--- test/ex_autolink_test.exs
+++ test/ex_autolink_test.exs
@@ -1,8 +1,4 @@
 defmodule ExAutolinkTest do
   use ExUnit.Case
   doctest ExAutolink
-
-  test "greets the world" do
-    assert ExAutolink.hello() == :world
-  end
 end
